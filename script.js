1f96adb6db054d2ec084e29445ebad7f4faa8fa5
akamimartin <akamimartin@gmail.com>
2015-08-14 01:02:24 -0700

    latest changes

---
+++ script.js
@@ -0,0 +1,8 @@
+ 
+$(document).ready(function() {
+    console.log("in the boo1 function");
+    $('.projects-img1').click(function(){
+        //$('.projects-img1').add('<a href="about.html"</a>');
+        alert("hi");
+    });  
+});
