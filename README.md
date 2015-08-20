ef4eda92aa1263da6fa79b23b5d4087e752f55de
akamimartin <akamimartin@gmail.com>
2015-08-20 16:39:02 -0700

    updated resume

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 This is my portfolio.
-Live Version: http://52.24.95.246/portfolio/
+Live Version: http://52.24.95.246/portfolio_n/
--- script.js
+++
@@ -1,8 +0,0 @@
- 
-$(document).ready(function() {
-    console.log("in the boo1 function");
-    $('.projects-img1').click(function(){
-        //$('.projects-img1').add('<a href="about.html"</a>');
-        alert("hi");
-    });  
-});
